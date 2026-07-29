cca6a5f6c059b85676e12315059a40fe39131508
XiSiAn916 <15513911153@163.com>
2026-07-29 10:27:52 +0800

    Fix imports and BrainStats reference

--- app/src/main/java/com/ombre/brain/MainActivity.kt
+++ app/src/main/java/com/ombre/brain/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.ombre.brain.core.AssociationEngine
 import com.ombre.brain.core.MemoryEngine
 import com.ombre.brain.security.AccessGuard
@@ -148,7 +154,7 @@ fun HomeScreen(
     modifier: Modifier = Modifier
 ) {
     val scope = rememberCoroutineScope()
-    var stats by remember { mutableStateOf(memoryEngine.BrainStats(0, 0, 0, 0, 0)) }
+    var stats by remember { mutableStateOf(MemoryEngine.BrainStats(0, 0, 0, 0, 0)) }
 
     LaunchedEffect(Unit) {
         stats = memoryEngine.getStats()
